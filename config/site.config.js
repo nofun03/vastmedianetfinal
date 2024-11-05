1e1e596125716fee99e2edc112dba0d4fa8eea31
nofun03 <127082138+nofun03@users.noreply.github.com>
2024-11-05 07:38:14 +0600

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -45,7 +45,7 @@ module.exports = {
   protectedRoutes: ['/vastmedia'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
-  email: 'mailto:',
+  email: 'mailto:contentmain@bntechauto.info',
 
   // [OPTIONAL] This is an array of names and links for setting your social information and links.
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
